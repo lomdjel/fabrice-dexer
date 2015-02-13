72f2f1b5f52262ed9bab19597cfc53935141dd0a
lomdjel <lomdjel@gmail.com>
2015-02-13 16:45:31 +0100

    envoi des fichiers

---
+++ index.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Dexer</title>
+
+    <!-- CSS -->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400italic,400">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Droid+Sans">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lobster">
+    
+    <!-- Bootstrap -->    
+    <link href="public/css/bootstrap.min.css" rel="stylesheet">
+    <link href="public/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="public/css/style.css">
+    
+    
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    	<script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+    
+    <!-- Favicon and touch icons -->
+    <link rel="shortcut icon" href="assets/ico/favicon.ico">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="public/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="public/ico/apple-touch-icon-114-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="public/ico/apple-touch-icon-72-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" href="public/ico/apple-touch-icon-57-precomposed.png">
+    
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    
+    <div class="container" style="margin-bottom: 50px;">
+
+    	<div class="row">
+			<div class="col-md-3" style="background-color: #F0F0F0;border-radius: 5px;">
+				<div>
+					<h4>Logo</h4>
+					<img src="..." alt="..." class="img-rounded">
+					<nav>
+						<ul>
+			                <li><a href="#">...</a></li>
+			                <li><a href="#">...</a></li>
+			                <li><a href="#">...</a></li>
+			                <li><a href="#">...</a></li>
+			                <li><a href="#">...</a></li>
+			            </ul>
+					</nav>
+					
+				</div>
+				
+				<hr>
+				<div>
+					<form class="form-horizontal">
+					  
+					  <div class="form-group">
+					    <div class=col-sm-10>
+					      <input type="email" class="form-control" id="inputEmail3" placeholder="Enter your email">
+					    </div>
+					  </div>
+					  
+					  <div class="form-group">
+					    <div class="col-sm-offset-2 col-sm-10">
+					      <p class="text-right" style="margin-right: 50px;"><button type="submit" class="btn btn-primary">Send</button></p>
+					    </div>
+					  </div>
+					  
+					</form>
+				</div>
+				
+				<div style="height: 800px;">
+				
+				</div>
+				
+			</div>
+		  	<div class="col-md-9">
+		  		
+		  		<header>
+			    	<p class="text-right"><a href="#">Login</a> | <a href="#">Sitemap</a></p>
+			    </header>
+		  		
+		  		<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+				  <!-- Indicators -->
+				  <ol class="carousel-indicators">
+				    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+				    <li data-target="#carousel-example-generic" data-slide-to="3"></li>
+				  </ol>
+				
+				  <!-- Wrapper for slides -->
+				  <div class="carousel-inner" role="listbox">
+				    
+				    <div class="item active">
+				      <img src="public/img/1.jpg">
+				      <div class="carousel-caption">
+				      </div>
+				    </div>
+				    
+				    <div class="item">
+				      <img src="public/img/2.jpg">
+				      <div class="carousel-caption">
+				      </div>
+				    </div>
+				    
+				    <div class="item">
+				      <img src="public/img/5.jpg">
+				      <div class="carousel-caption">
+				      </div>
+				    </div>
+				    
+				    <div class="item">
+				      <img src="public/img/6.jpg">
+				      <div class="carousel-caption">
+				      </div>
+				    </div>
+				    
+				  </div>
+				  
+				  <!-- Controls -->
+				  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+				    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+				    <span class="sr-only">Previous</span>
+				  </a>
+				  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+				    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+				    <span class="sr-only">Next</span>
+				  </a>
+				</div>
+				  
+		  		<div class="navbar">
+		  			<ul class="nav nav-pills" style="margin-left: 12px;">
+		  				<li><a href="#">home</a></li>
+		  				<li><a href="#">A propos</a></li>
+		  				<li><a href="#">services</a></li>
+		  				<li><a href="#">cond° générales</a></li>
+		  				<li><a href="#">Contact</a></li>
+		  				<li><a href="#">Questions</a></li>
+		  			</ul>
+		  		</div>
+		  		
+		  		<!-- Services -->
+		        <div class="what-we-do">
+		            <div class="container-fluid">
+		            <div class="row">
+		                <div class="service col-md-4 col-md-4-bis">
+		                    <div class="icon-awesome">
+		                        <i class="icon-eye-open"></i>
+		                    </div>
+		                    <h4>Type de Déméngament</h4>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+		                    <a href="services.html">Read more</a>
+		                </div>
+		                <div class="service col-md-4 col-md-4-bis">
+		                    <div class="icon-awesome">
+		                        <i class="icon-table"></i>
+		                    </div>
+		                    <h4>Comment on fait?</h4>
+		                    <p>Etape par étape - Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+		                    <a href="services.html">Read more</a>
+		                </div>
+		                <div class="service col-md-4 col-md-4-bis">
+		                    <div class="icon-awesome">
+		                        <i class="icon-magic"></i>
+		                    </div>
+		                    <h4>Demande en Ligne</h4>
+		                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+		                    <a href="services.html">Read more</a>
+		                </div>
+		            </div>
+		            </div>
+		        </div>
+		        
+		        <div class="row" style="margin-top: 15px">
+				  <div class="col-xs-6">
+				  	<div class="service-bis">
+	                    <div class="icon-awesome">
+	                        <i class="icon-table"></i>
+	                    </div>
+	                    <h4>Nouvelles de l'entreprise</h4>
+	                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+	                    <ul>
+	                    	<li>Lorem ipsum dolor</li>
+	                    	<li>Lorem ipsum dolor</li>
+	                    	<li>Lorem ipsum dolor</li>
+	                    	<li>Lorem ipsum dolor</li>
+	                    </ul>
+	                    <a href="services.html">Read more</a>
+	                </div>
+				  </div>
+				  <div class="col-xs-6">
+				  	<div class="service-bis" style="color: #FFF; background-color: #5bc0de;">
+	                    <div class="icon-awesome">
+	                        <i class="icon-table"></i>
+	                    </div>
+	                    <h4>Pourquoi les gens choississent Dexer</h4>
+	                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et...</p>
+	                    <a href="services.html" style="color: #fff">Read more</a>
+	                </div>
+				  </div>
+				</div>
+		  		
+		  	</div>
+		</div>
+    </div>
+    
+     <!-- Footer -->
+        <footer class="footer">
+            <div class="container">
+                
+                <div class="row">
+                <p class="text-center"></p>
+                </div>
+                
+                <div>
+				  <p class="text-center">&copy;2015 dexer Tous droits réservés. Réalisation : Lomdjel</p>
+				</div>
+                 
+            </div>
+        </footer>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="public/js/bootstrap.min.js"></script>
+    <script src="public/js/bootstrap.js"></script>
+    
+  </body>
+</html>
